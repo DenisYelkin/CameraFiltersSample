edea4a3536e992ea0464f2b39c6e8cb4ea257b6d
Denis Elkin <elkin@haulmont.com>
2020-01-20 20:04:46 +0400

    Init project

---
+++ app/src/main/java/com/elkin/sample/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.elkin.sample.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.elkin.sample.R
+
+/**
+ * @author elkin
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
